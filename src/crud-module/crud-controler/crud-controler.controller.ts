2ac3ceb0dcca85bc6f3787f47ec1e24128320f34
ahmedalphasquad123 <ahmedali.alphasquad@gmail.com>
2024-02-01 16:06:13 +0500

    basic crud

--- src/crud-module/crud-controler/crud-controler.controller.ts
+++ src/crud-module/crud-controler/crud-controler.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import{CrudServiceService} from '../crud-service/crud-service.service'
-import {UserDto} from '../../Dtos/crud-dto'
+import {UserDto} from '../../dtos/crud-dto'
 @Controller('crud')
 export class CrudControlerController {
     constructor(private CrudService:CrudServiceService){
@@ -12,6 +12,18 @@ export class CrudControlerController {
     }
     @Post("addUser")
     addUserRoute(@Body() User:UserDto){
-        return  this.CrudService.getCrud()
+        return  this.CrudService.addCrud(User)
     }    
+    @Get("oneUser/:id")
+    getUserbyId(@Param() prams){
+        return this.CrudService.getOneUser(prams.id)
+    } 
+    @Delete("deleteOne/:id")
+    deletebyId(@Param() prams){
+        return this.CrudService.deleteOne(prams.id)
+    }   
+    @Put("updateOne/:id")
+    updateOne(@Param() prams, @Body() User:UserDto){
+        return this.CrudService.updateUser(prams.id,User)
+    }   
 }
--- src/crud-module/crud-service/crud-service.service.ts
+++ src/crud-module/crud-service/crud-service.service.ts
@@ -1,22 +1,109 @@
 import { Injectable } from '@nestjs/common';
+import {DbServiceService} from '../../common-services/db-service/db-service.service'
+import { User, Prisma } from '@prisma/client';
+
+
+interface user {
+    email : string,
+    password : string,
+    name : string
+
+}
 
 @Injectable()
 export class CrudServiceService {
+    constructor(private prisma: DbServiceService){
 
-    getCrud(){
-       return {
-        status:200,
-        message:'works fine',
-        resposne:'hello from service' 
-        } 
+    }
+    async getCrud(){
+        try{
+        const userList = await this.prisma.user.findMany()
+        return {
+            status:200,
+            message:'list of all user',
+            resposne:userList 
+            } }
+        catch(err){
+            return{
+                status:500,
+                message:'error in getting all user',
+                resposne:err
+                } 
+        }
     }
 
-    addCrud(){
+    async getOneUser(id:number){
+        try{
+        const userList = await this.prisma.user.findFirst(
+            {where: { id: Number(id) }})
+           return {
+            status:200,
+            message:'one user',
+            resposne:userList 
+            }
+        } 
+        catch(err){
+            return{
+                status:500,
+                message:'error in getting one user',
+                resposne:err
+                } 
+        }
+    }
+    
+    async deleteOne(id:number){
+        try{
+        const userList = await this.prisma.user.delete(
+            {where: { id: Number(id) }})
+           return {
+            status:200,
+            message:'user deleted',
+            resposne:userList 
+            }}
+            catch(err){
+                return{
+                    status:500,
+                    message:'error in deleting user',
+                    resposne:err
+                    } 
+            } 
+    }
+
+    async addCrud(data:any){
+        try{
+        const newUser = await this.prisma.user.create({data})
         return {
-         status:200,
-         message:'works fine',
-         resposne:[] 
-         } 
+         status:201,
+         message:'user created',
+         resposne:newUser
+         } }
+         catch(err){
+            return{
+                status:500,
+                message:'error in creating user',
+                resposne:err
+                } 
+         }
      }
- 
+
+ async updateUser(id:number,data:any){
+        try{
+        const newUser = await this.prisma.user.update({
+            where: { id: Number(id) },
+            data,
+          })
+        return {
+         status:201,
+         message:'user created',
+         resposne:newUser
+         } }
+         catch(err){
+            return{
+                status:500,
+                message:'error in creating user',
+                resposne:err
+                } 
+         }
+     }
+     
 }
